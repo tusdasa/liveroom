7e6a43dc777a03a6a9e7e4c7248f3fa4a26c90f4
tusdasa <tusdasa@163.com>
2022-02-20 01:10:00 +0800

    添加IM SDK和播放器 TCPlayerLite

--- src/main.js
+++ src/main.js
@@ -2,5 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {TcPlayer} from "@/assets/TcPlayer-module-2.4.1.js";
+import DPlayer from 'dplayer';
+import TIM from 'tim-js-sdk';
+import TIMUploadPlugin from 'tim-upload-plugin';
 
-createApp(App).use(store).use(router).mount('#app')
+
+let tim = TIM.create({SDKAppID: 1400537309}); 
+tim.setLogLevel(0);
+tim.registerPlugin({'tim-upload-plugin': TIMUploadPlugin});
+
+const app = createApp(App)
+// 挂载 TCplayer
+app.config.globalProperties.$TcPlayer = TcPlayer
+// 挂载DPlyaer
+app.config.globalProperties.$DPlayer = DPlayer
+
+// 挂载IM
+app.config.globalProperties.$TIM = tim
+
+// 配置插件
+app.use(store)
+app.use(router)
+app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="id_test_video"></div>
+    <div id="id_test_1"></div>
   </div>
 </template>
+<script>
+export default {
+  name: 'AboutView',
+  data(){
+    return{
+      palyer:null
+    }
+  },
+  setup(){
+    console.log("0")
+  },
+  created() {
+    console.log("1")
+  },
+  mounted() {
+    this.palyer = new this.$TcPlayer('id_test_video', {
+      "mp4":"https://1400329073.vod2.myqcloud.com/ff439affvodcq1400329073/7a9b2b565285890804459281865/jlm6QRPE4WUA.mp4",
+      "autoplay" : true,
+      "width" :  '640',
+      "height" : '480',
+      live:true,
+      listener: function (e) {
+        console.log(e)
+      }
+    });
+
+    new this.$DPlayer({
+    container: document.getElementById('id_test_1'),
+    autoplay: true,
+    lang:'zh-cn',
+    
+    video: {
+        url: 'https://1400329073.vod2.myqcloud.com/ff439affvodcq1400329073/7a9b2b565285890804459281865/jlm6QRPE4WUA.mp4'
+    }});
+  },
+  beforeMount() {
+  },
+  methods(){
+  },
+  components: {
+  }
+}
+</script>
