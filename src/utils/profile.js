152bbe08798cd4e024dc990ad66f49d527c24e3c
tusdasa <tusdasa@163.com>
2022-06-05 00:57:13 +0800

    添加两个工具类

---
+++ src/utils/profile.js
@@ -0,0 +1,52 @@
+export function getToken(){
+    let profile = getProfile();
+    if (profile){
+        return profile.token
+    }else {
+        return null
+    }
+}
+
+export function getProfile(){
+    let profile = localStorage.getItem("profile");
+    if (profile == null || profile === ''){
+        profile =  sessionStorage.getItem("profile");
+    }
+
+    if (profile == null || profile === ''){
+        return  null
+    }
+
+    let obj = JSON.parse(profile);
+
+    if (new Date().getTime() - obj.created >= obj.expires){
+        return null
+    }else {
+        return obj
+    }
+
+}
+
+export function setProfile(profile){
+    let obj = {
+        value: profile,
+        expires: 604800,//7*24*3600
+        created: new Date().getTime()
+    }
+    /*
+    {
+    "uid": 1,
+    "accountId": "user_lSc6tDPCRj",
+    "profile": {
+        "profileId": 1,
+        "nickname": "user_47925275",
+        "avatar": "https://xiaozhibo-1257166515.cos.ap-guangzhou.myqcloud.com/0001/1.jpg"
+    },
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJhdWQiOiJ1c2VyX2xTYzZ0RFBDUmoiLCJpc3MiOiJBdXRoZW50aWNhdGlvbiIsImlkIjoxLCJleHAiOjE2NTQzNjI3OTYsImlhdCI6MTY1NDM1OTE5Nn0.eM-yerBzM2Rz8ezhmwWBT8Dd0qphT8Lh-tdM1V7P0qqzDHKGNVZvDtzTFx9fionC"
+}
+
+     */
+    localStorage.setItem("profile", JSON.stringify(obj));
+    sessionStorage.setItem("profile",JSON.stringify(obj));
+
+}
---
+++ src/utils/request.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+import {getToken} from "@/utils/profile"
+
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.baseURL="https://dev1.tusdasa.net"
+
+function heandleData(isSuccess, msg, data){
+    return {
+        "isSuccess": isSuccess,
+        "message": msg,
+        "data": data
+    }
+}
+
+function heandleError(res){
+    // 确保业务代码响应正确
+    if(res.status === 200){
+        // 判断业务逻辑响应码, 非200 未失败
+        if(res.data.code === 200){
+            // 封装数据
+            if (res.data.model != null){
+                return heandleData(
+                    true,
+                    res.data.message,
+                    res.data.model
+                )
+            }
+            if (res.data.models != null){
+                return  heandleData(
+                    true,
+                    res.data.message,
+                    res.data.models
+                )
+            }
+        }else{
+            // 失败
+            return heandleData(
+                    false,
+                    res.data.message,
+                    null
+                )
+        }
+    }else{
+        // 服务端返回非正确的错误码
+        // res.statusText
+        return heandleData(
+                false,
+                res.statusText,
+                null
+            )
+    }
+}
+
+
+
+/**
+ * 登录
+ * */
+export function userLogin(mobile, password){
+
+    return new Promise((resolve, reject)=>{
+    axios.post("/auth/login", {
+      "mobile": mobile,
+      "password": password
+    })
+    .then(res => {
+        // 确保是正确响应
+        resolve(heandleError(res))
+    })
+    .catch(err => {
+        reject(heandleData(false, err,null))
+    });
+    })
+}
+
+export function getRoomList(page, size){
+    return new Promise((resolve, reject)=>{
+        axios.get("/live/all?page="+page+"&size="+size,{
+            headers: {'Authorization': getToken()}
+        }).then(res =>{
+            resolve(heandleError(res))
+        }).catch(err=>{
+            reject(heandleData(false, err,null))
+        })
+    })
+}
